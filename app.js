1416ebc11ae98751fa4a44245a0e1bcdee4f3333
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-02-28 17:20:46 -0500

    added maganer questions

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
-const { writeFile, copyFile } = require('./utils/generateHTML.js');
+const generateHTML = require('./src/page-template.js');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const fs = require('fs');
+
+
+// manager questions
+const addManager = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the team manager's name? (Required)",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's name!");
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the team manager's ID? (Required)",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's ID!");
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is manager's email address?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter the manager's email address!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: "What is manager's office number?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("What is manager's office number?");
+                return false; 
+            }
+        }
+    },
+])
+};
+addManager();
